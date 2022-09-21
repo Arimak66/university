ed2e9bbd5f3f59d98cc74b482d37a2b34fcfa477
Ari Makkonen <ari.makkonen@pp3.inet.fi>
2022-09-21 10:25:22 +0300

    Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System;
+
+namespace university.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        public LoginController(Database db)
+        {
+            Db = db;
+        }
+        // POST api/login
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]User body)
+        {
+            Console.WriteLine(body.username);
+            Console.WriteLine(body.password);
+            await Db.Connection.OpenAsync();
+            var query = new Login(Db);
+            var dataFromDb = await query.GetPassword(body.username);
+   
+            if (dataFromDb is null || ! BCrypt.Net.BCrypt.Verify(body.password, dataFromDb.password))
+            {
+                // authentication failed
+                return new OkObjectResult(false);
+            }
+            else
+            {
+                // authentication successful
+                return new OkObjectResult(dataFromDb.identity);
+            }
+            
+        }
+
+    
+
+        public Database Db { get; }
+    }
+}

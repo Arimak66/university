e86796357b56de84d9f7fc50499997ba278f6bd2
Ari Makkonen <ari.makkonen@pp3.inet.fi>
2022-10-12 20:16:24 +0300

    Herokukorjausta

--- Database.cs
+++ Database.cs
@@ -12,7 +12,7 @@ namespace university
         {
             string myConnectionString;
             //myConnectionString = "server=192.168.1.112;uid=amakkonen;pwd=Eirene1234!;database=university";
-            Connection = new MySqlConnection(System.Environment.GetEnvironmentVariable("CLEARDB_WHITE_URL"));
+            Connection = new MySqlConnection(System.Environment.GetEnvironmentVariable("DATABASE_URL"));
             //Connection = new MySqlConnection(myConnectionString);
         }
 
